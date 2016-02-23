816734934966f5e0c59e63995e81ebd62857d313
Fabiano Bizarro <fabiano.bizarro@outlook.com>
2016-02-22 22:34:41 -0300

    Finalização da parte de cadastro dos grupos

--- app/controllers/GrupoController.js
+++ app/controllers/GrupoController.js
@@ -5,7 +5,7 @@ var GrupoRepository = require('../repositories/GrupoRepository'),
 
 
 exports.listarGrupos = function (req, res, next) {
-    repository.getAll((err, grupos)=>{
+    repository.getAll((err, grupos) => {
         if (err)
             return next(err);
         else
@@ -16,13 +16,13 @@ exports.listarGrupos = function (req, res, next) {
 exports.adicionarGrupo = function (req, res, next) {
 
     let grupo = req.body;
-    
+
     grupo.administradores = [];
     grupo.integrantes = [];
     grupo.administradores.push(req.requestUser._id);
     grupo.integrantes.push(req.requestUser._id);
-    
-    repository.add(grupo, (err)=>{
+
+    repository.add(grupo, (err) => {
         if (err)
             return next(err);
         else
@@ -38,14 +38,14 @@ exports.exibirGrupo = function (req, res, next) {
 }
 
 exports.alterarGrupo = function (req, res, next) {
-    
+
     let novoGrupo = req.body;
     let grupo = req.grupo;
-    
+
     grupo.nome = novoGrupo.nome || grupo.nome;
     grupo.descricao = novoGrupo.descricao || grupo.descricao;
     grupo.grupoPublico = novoGrupo.grupoPublico || grupo.grupoPublico;
-    
+
     repository.update(grupo, (err) => {
         if (err)
             return next(err);
@@ -59,6 +59,26 @@ exports.alterarGrupo = function (req, res, next) {
 
 exports.excluirGrupo = function (req, res, next) {
 
+    var grupo = req.grupo;
+    
+
+    if (grupo.administradores.indexOf(req.requestUser._id) == -1) {
+        // Usuário não está no grupo de administradores
+        return next(new Error('O usuário não possui permissão para exluir este grupo pois o mesmo não é adminsitrador'));
+    }
+    else {
+        repository.delete(grupo, (err) => {
+            if (err)
+                return next(err);
+                
+            res.json({
+                sucesso: true,
+                mensagem: 'O grupo foi excluído com sucesso.'
+            });
+
+        })
+    }
+
 }
 
 exports.obterGrupoPorId = function (req, res, next, id) {
@@ -66,7 +86,7 @@ exports.obterGrupoPorId = function (req, res, next, id) {
     repository.findById(id, (err, grupo) => {
         if (err) return next(err);
         if (!grupo) return next(new Error(`Não foi possível encontrar um grupo com o id ${id}`));
-        
+
         req.grupo = grupo;
         next();
     });
