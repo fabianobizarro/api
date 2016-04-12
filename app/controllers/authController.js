7ae304614f7b925453db7663ea0d591fc28b1bd5
Fabiano Bizarro <fabiano.bizarro@outlook.com>
2016-04-11 23:38:16 -0300

    username and password validation before authController actions

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -8,24 +8,41 @@ exports.signIn = function(req, res, next) {
     let usuario = req.body.login;
     let senha = req.body.senha;
 
+    // if (usuario == '' || usuario == null || usuario == undefined) {
+    //     res.statusCode = 400;
+    //     return next(new Error("O login do usuário deve ser informado."));
+    // }
+
+    // if (senha == '' || senha == null || senha == undefined) {
+    //     res.statusCode = 400;
+    //     return next(new Error("A senha do usuário deve ser informado."));
+    // }
+
 
     repo.findOne({ login: usuario }, (err, usuario) => {
 
         if (err)
             return next(err);
 
-        if (!usuario) // Verifica se o usuário existe
-            return next(new Error('Usuário/Senha inválidos.'));
+        if (!usuario) { // Verifica se o usuário existe
+            res.status(401).json({
+                sucesso: false,
+                mensagem: 'Usuário/Senha inválidos.'
+            });
+        }
         else {
             if (usuario.senha !== usuario.hashPassword(senha)) // Verifica se a senha está correta
             {
-                return next(new Error('Usuário/Senha inválidos.'))
+                res.status(401).json({
+                    sucesso: false,
+                    mensagem: 'Usuário/Senha inválidos.'
+                });
             }
             else {
                 // se tudo estiver ok, gera o token e retorna ao usuário
                 let token = authService.signIn(usuario);
 
-                res.json({
+                res.status(200).json({
                     sucesso: true,
                     mensagem: 'Usuário autenticado com sucesso.',
                     token: token
@@ -122,4 +139,20 @@ exports.checkToken = function(req, res, next) {
             mensagem: 'Nenhum token foi informado'
         });
     }
+}
+
+exports.validarUsuarioSenha = function (req, res, next) {
+    let usuario = req.body.login;
+    let senha = req.body.senha;
+
+    if (usuario == '' || usuario == null || usuario == undefined) {
+        res.statusCode = 400;
+        return next(new Error("O login do usuário deve ser informado."));
+    }
+    else
+        if (senha == '' || senha == null || senha == undefined) {
+            res.statusCode = 400;
+            return next(new Error("A senha do usuário deve ser informado."));
+        }
+    return next();
 }
--- app/routes/authRoutes.js
+++ app/routes/authRoutes.js
@@ -7,6 +7,7 @@ module.exports = function(){
     let controller = require("../controllers/authController");
     var rotas = express.Router();
     
+    rotas.use(controller.validarUsuarioSenha)
     rotas.post('/signIn', controller.signIn);
     rotas.post('/signIn/admin', controller.signInAdmin);
     rotas.post('/signUp', controller.signUp);
