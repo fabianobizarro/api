f00e00c50d88f38609703ac3d00b1ad5cc06579c
Fabiano Bizarro <fabiano.bizarro@outlook.com>
2016-06-06 17:39:17 -0300

    rota /usuario/info adicionada

--- app/routes/usuarioRoutes.js
+++ app/routes/usuarioRoutes.js
@@ -1,21 +1,26 @@
 'use strict';
 module.exports = function () {
-    
+
     let express = require("express");
     let controller = require('../controllers/UsuarioController');
 
     let rotas = express.Router();
-    
+
     rotas.route('/usuario')
         .get(controller.listarUsuarios)
         .post(controller.adicionarUsuario);
+        
+    rotas.route('/usuario/info')
+        .get(controller.infoUsuario);
 
     rotas.route("/usuario/:idUsuario")
         .get(controller.dadosUsuario)
         .put(controller.alterarUsuario)
         .delete(controller.excluirUsuario);
 
+
+
     rotas.param('idUsuario', controller.obterUsuarioPorId);
-    
+
     return rotas;
 }
