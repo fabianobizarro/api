dd8482c0de73d0367187f0c24968aeb796477b6d
Fabiano Bizarro <fabiano.bizarro@outlook.com>
2016-08-10 15:45:11 -0300

    método no ReportController desativado

--- app/controllers/ReportController.js
+++ app/controllers/ReportController.js
@@ -1,5 +1,4 @@
 var dateService = require('../services/dateService');
-var categoriaNoticiaRepository = require('../repositories/CategoriaNoticiaRepository');
 var reportService = require('../services/reportService');
 require('../services/Array');
 
@@ -9,50 +8,50 @@ exports.index = function (req, res, next) {
 
 exports.qt_noticiasPorCategoria = function (req, res, next) {
 
-    var categoriaRepo = new categoriaNoticiaRepository();
+    return res.json([]);
 
-    var dataInicio = dateService.dataFormatada(req.params.dti);
-    var dataTermino = dateService.dataFormatada(req.params.dtt);
+    // var dataInicio = dateService.dataFormatada(req.params.dti);
+    // var dataTermino = dateService.dataFormatada(req.params.dtt);
 
-    console.log(dataInicio)
-    console.log(dataTermino)
+    // console.log(dataInicio)
+    // console.log(dataTermino)
 
-    if (dataInicio && dataTermino) {
+    // if (dataInicio && dataTermino) {
 
-        dataInicio = new Date(dataInicio.ano, dataInicio.mes - 1, dataInicio.dia);
-        dataTermino = new Date(dataTermino.ano, dataTermino.mes - 1, dataTermino.dia);
+    //     dataInicio = new Date(dataInicio.ano, dataInicio.mes - 1, dataInicio.dia);
+    //     dataTermino = new Date(dataTermino.ano, dataTermino.mes - 1, dataTermino.dia);
 
 
-        categoriaRepo.getAll((err, categorias) => {
+    //     categoriaRepo.getAll((err, categorias) => {
 
-            if (err) return next(err);
+    //         if (err) return next(err);
 
-            categorias = categorias.map(item => {
-                return {
-                    _id: item._id, nome: item.nome
-                }
-            });
-            console.log(dataInicio)
-            console.log(dataTermino)
-            reportService.quanditateNoticiaPorCategoria(dataInicio, dataTermino, (err, result) => {
-                if (err)
-                    return next(err);
+    //         categorias = categorias.map(item => {
+    //             return {
+    //                 _id: item._id, nome: item.nome
+    //             }
+    //         });
+    //         console.log(dataInicio)
+    //         console.log(dataTermino)
+    //         reportService.quanditateNoticiaPorCategoria(dataInicio, dataTermino, (err, result) => {
+    //             if (err)
+    //                 return next(err);
 
-                console.log(categorias)
-                console.log(result)
+    //             console.log(categorias)
+    //             console.log(result)
 
-                result.forEach(item => {
-                    item.nome = categorias.where(i => { return i._id == item._id })[0].nome;
-                });
+    //             result.forEach(item => {
+    //                 item.nome = categorias.where(i => { return i._id == item._id })[0].nome;
+    //             });
 
-                console.log(result);
-                res.json(result);
-            });
+    //             console.log(result);
+    //             res.json(result);
+    //         });
 
-        });
+    //     });
 
-    }
-    else
-        return next(new Error('A requisição possui parâmetros inválidos'));
+    // }
+    // else
+    //     return next(new Error('A requisição possui parâmetros inválidos'));
 
 };
