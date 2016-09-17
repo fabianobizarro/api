3142b13cad3c55c8f4500dbee5ccbb57a684a29c
Fabiano Bizarro <Fabiano Bizarro>
2016-09-17 16:46:46 -0300

    Modelos antigos removidos

--- app/_models/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	ModelService: require('./ModelService.js'),
-	CategoriaNoticia: require('./CategoriaNoticia.js'),
-	Noticia: require('./Noticia.js'),
-	Usuario: require('./Usuario.js'),
-	Grupo: require('./Grupo.js')
-}
